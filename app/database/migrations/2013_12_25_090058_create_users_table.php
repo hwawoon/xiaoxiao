63eacf4d41671ff26fc0817b5dfb49aa4d4e4d6d
kimhwawoon <kimhwawoon@gmail.com>
2014-01-10 16:39:07 +0800

    add users table structure

--- app/database/migrations/2013_12_25_090058_create_users_table.php
+++ app/database/migrations/2013_12_25_090058_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration {
             $table->string('email')->unique();
             $table->string('name');
             $table->string('password');
+            $table->string('avatar')->default("avatar/default.jpg");
             $table->timestamps();
         });
 	}
