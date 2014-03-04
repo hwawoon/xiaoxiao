9c5919235e505d00676e8c95e6a0626dfc5de4b5
kimhwawoon <kimhwawoon@gmail.com>
2014-03-04 18:30:58 +0800

    #chg# compress js and css

---
+++ public/js/user.security-1393435577198.js
@@ -0,0 +1,1 @@
+$(function(){$("#passwordForm").validate({rules:{old_password:{required:true},new_password:{required:true},new_password_confirmation:{required:true,equalTo:"#new_password"}},messages:{old_password:{required:"请输入当前密码"},new_password:{required:"请输入新密码"},new_password_confirmation:{required:"请输入确认密码",equalTo:"两次输入密码不一致"}}});});
