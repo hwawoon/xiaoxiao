1187ef9a796376221edfb3c7e8a6d49c44d20135
kimhwawoon <kimhwawoon@kimhwawoon@gmail.com>
2014-03-07 16:22:34 +0800

    add meta content

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="@yield('description')">
+    <meta name="author" content="kimhwawoon, gaoxiaowa.com, 搞笑娃">
+    <meta name="keywords" content="@yield('keywords')">
     <link rel="shortcut icon" href="{{URL::to('/')}}/favicon.ico">
     <title>@yield('title')</title>
     <!-- Bootstrap core CSS -->
