f1da6463bea3b28841c1ef0b466cb16c2542236a
kimhwawoon <kimhwawoon@gmail.com>
2014-03-18 13:34:48 +0800

    change sharebar

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -38,4 +38,9 @@ navbar-fixed-top
 {{ HTML::script('packages/underscore.min.js') }}
 {{ HTML::script('js/home-1394354121514.js') }}
 @include('includes.js-article')
+<script type="text/javascript" id="bdshare_js" data="type=tools" ></script>
+<script type="text/javascript" id="bdshell_js"></script>
+<script type="text/javascript">
+    document.getElementById("bdshell_js").src = "http://bdimg.share.baidu.com/static/js/shell_v2.js?cdnversion=" + Math.ceil(new Date()/3600000);
+</script>
 @stop
--- app/views/includes/article-section.blade.php
+++ app/views/includes/article-section.blade.php
@@ -36,16 +36,20 @@
             @endif
         </div>
         <div class="row artshare">
-            <div style="float: left;width: 50px;line-height: 50px;font-size: 16px;font-weight: bold;margin-right: 10px;">分享到</div>
-            <div class="bdsharebuttonbox" style="margin-top: -5px;">
-                <a title="分享到QQ空间" href="#" class="bds_qzone" data-cmd="qzone"></a>
-                <a title="分享到新浪微博" href="#" class="bds_tsina" data-cmd="tsina"></a>
-                <a title="分享到腾讯微博" href="#" class="bds_tqq" data-cmd="tqq"></a>
-                <a title="分享到人人网" href="#" class="bds_renren" data-cmd="renren"></a>
-                <a title="分享到微信" href="#" class="bds_weixin" data-cmd="weixin"></a>
-                <a href="#" class="bds_more" data-cmd="more"></a>
+            <!-- Baidu Button BEGIN -->
+            <div id="bdshare" class="bdshare_t bds_tools_24 get-codes-bdshare" data="
+          {
+            'url':'{{URL::to('/art').'/'.$article->id}}',
+            'pic':'{{URL::to('/')}}/{{$article->imgpath}}',
+            'text':'{{$article->title}}  --来自《搞笑娃》'
+          }">
+                <a class="bds_qzone"></a>
+                <a class="bds_tsina"></a>
+                <a class="bds_tqq"></a>
+                <a class="bds_renren"></a>
+                <span class="bds_more"></span>
             </div>
-            <script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"32"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
+            <!-- Baidu Button END -->
         </div>
     </div>
     <div class="col-xs-2">
